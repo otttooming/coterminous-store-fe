bbc21104237f13725404aab75536d6082569bbe5
Ott Tooming <OttTooming@gmail.com>
2017-06-18 01:38:50 +0300

    Add product API request by slug

--- pages/product.js
+++ pages/product.js
@@ -5,12 +5,12 @@ import Page from '../layouts/main'
 
 export default class extends React.Component {
   static async getInitialProps ({ query: { id } }) {
-    const res = await fetch('https://spiceflow.net.ee/wp-json/wc/v2/products/' + id + '?consumer_key=ck_27c96da6c28aa2d9022ef35d824607189f76b549&consumer_secret=cs_10ed7d30416d147277f0c07f8e43e6f98e0d2bf9')
+    const res = await fetch('https://spiceflow.net.ee/wp-json/wc/v2/products/?consumer_key=ck_27c96da6c28aa2d9022ef35d824607189f76b549&consumer_secret=cs_10ed7d30416d147277f0c07f8e43e6f98e0d2bf9&slug=' + id)
     const json = await res.json()
 
     return {
         id: id,
-        product: json
+        product: json[0]
      }
   }
 
