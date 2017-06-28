e113c88e855379fc1bceefac6d8d37c8edf50c4c
Ott Tooming <OttTooming@gmail.com>
2017-06-29 00:31:31 +0300

    Add async getInitialProps

--- pages/product.js
+++ pages/product.js
@@ -7,17 +7,33 @@ import Page from '../layouts/main'
 
 import Product from '../components/product'
 
+function  fetchData(query) {
+
+  const id = query.slug
+
+  let variationsJson = {}
+
+  const resp = new Promise(resolve => {
+    fetch(api.buildUrl({ paths: [api.WC, 'products'], parameters: ['slug=' + id] })).then(value => {
+      value.json().then(value => {
+
+        resolve({
+          id: query.slug,
+          product: value[0],
+          variations: variationsJson,
+          varUrl: api.buildUrl({ paths: [api.WC, 'products', value[0].id, 'variations'] })
+        })
+      })
+
+    })
+  })
+
+  return resp
+}
+
 export default class extends React.Component {
   static async getInitialProps({ query, res }) {
-    const id = query.slug
-
-    const resp = await fetch(api.buildUrl({ paths: [api.WC, 'products'], parameters: ['slug=' + id] }))
-    const json = await resp.json()
-
-    return {
-      id: query.slug,
-      product: json[0]
-    }
+    return await fetchData(query)
   }
 
   render() {
