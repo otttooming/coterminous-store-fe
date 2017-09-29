20fd6899f4f5acc57273d42006ebc1332224377b
Ott Tooming <OttTooming@gmail.com>
2017-09-29 21:53:20 +0300

    Update code style for root components

--- routes.js
+++ routes.js
@@ -1,10 +1,10 @@
-const nextRoutes = require('next-routes')
-const routes = module.exports = nextRoutes()
+const nextRoutes = require('next-routes');
+const routes = module.exports = nextRoutes();
 
-routes.add('cart', '/cart/')
-routes.add('checkout', '/checkout/')
-routes.add('blog', '/blog/:slug?')
-routes.add('product', '/product/:slug?')
-routes.add('products', '/products/:slug?')
-routes.add('about', '/about-us/:foo(bar|baz)')
-routes.add('order', '/order/:slug?')
+routes.add('cart', '/cart/');
+routes.add('checkout', '/checkout/');
+routes.add('blog', '/blog/:slug?');
+routes.add('product', '/product/:slug?');
+routes.add('products', '/products/:slug?');
+routes.add('about', '/about-us/:foo(bar|baz)');
+routes.add('order', '/order/:slug?');
