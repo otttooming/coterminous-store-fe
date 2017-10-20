29acc9e7fb2fc4c9892012215b37ae2fd853a49c
Ott Tooming <OttTooming@gmail.com>
2017-10-20 23:05:39 +0300

    Refactor CategoriesListing

--- src/components/categoriesListing/CategoriesListing.tsx
+++ src/components/categoriesListing/CategoriesListing.tsx
@@ -1,33 +1,42 @@
-import * as React from 'react';
+import * as React from "react";
 
 interface Props {
-  change: (id) => void;
-  categories: {
-    id: number;
-    name: string;
-  }[];
+  categories: CategoryProps[];
+  change: (id: number) => void;
+  handleCategoryChange(id: string): void;
+}
+
+interface CategoryProps {
+  id: number;
+  name: string;
+  count: number;
+  description: string;
 }
 
 export default class CategoriesListing extends React.Component<Props, any> {
-  constructor(props) {
+  constructor(props: Props) {
     super(props);
   }
 
-  handleCategoryChange = (id) => {
+  handleCategoryChange = (id: number) => {
     this.props.change(id);
-  }
+  };
 
   render() {
     const { categories } = this.props;
 
     const categoriesListing = categories.map((item, index) => {
+      const { id, name, count, description } = item;
+
       return (
-        <div key={index} className="cat-list__group ">
+        <div key={index} className="cat-list__group">
           <h2 className="cat-list__title">
-            <a href="#" onClick={() => this.handleCategoryChange(item.id)}>
-              <span className="cat-list__name">{item.name}</span>
-              <span className="cat-list__desc" />
-              <span className="cat-list__count">4</span>
+            <a href="#" onClick={() => this.handleCategoryChange(id)}>
+              <span className="cat-list__name">{name}</span>
+              {!!description && (
+                <span className="cat-list__desc">{description}</span>
+              )}
+              <span className="cat-list__count">{count}</span>
             </a>
 
             <div className="cat-list__subcat-control control__items">
@@ -38,7 +47,12 @@ export default class CategoriesListing extends React.Component<Props, any> {
                 height={25}
                 viewBox="0 0 501.5 501.5"
               >
-                <g><path fill="currentColor" d="M199.33 410.622l-55.77-55.508L247.425 250.75 143.56 146.384l55.77-55.507L358.44 250.75z" /></g>
+                <g>
+                  <path
+                    fill="currentColor"
+                    d="M199.33 410.622l-55.77-55.508L247.425 250.75 143.56 146.384l55.77-55.507L358.44 250.75z"
+                  />
+                </g>
               </svg>
             </div>
           </h2>
@@ -48,8 +62,6 @@ export default class CategoriesListing extends React.Component<Props, any> {
       );
     });
 
-    return (
-      <ul className="menu">{categoriesListing}</ul>
-    );
+    return <ul className="menu">{categoriesListing}</ul>;
   }
 }
