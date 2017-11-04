d1e8357b77e3869c7598e4606a23e17326321f03
Ott Tooming <OttTooming@gmail.com>
2017-11-04 19:05:38 +0200

    Refactor ProductsListing to stateless functional component

--- src/components/productsListing/ProductsListing.tsx
+++ src/components/productsListing/ProductsListing.tsx
@@ -18,58 +18,50 @@ interface ProductItemProps {
   price_html: string;
 }
 
-export default class ProductsListing extends React.Component<Props, any> {
-  constructor(props: Props) {
-    super(props);
-  }
+const ProductsListing = ({ products }: Props) => {
+  const listProducts = products.map((item, index) => {
+    const { name, slug, price, price_html } = item.product;
 
-  render() {
-    const { products } = this.props;
-
-    const listProducts = products.map((item, index) => {
-      const { name, slug, price, price_html } = item.product;
-
-      const { images } = item;
-
-      return (
-        <li
-          key={index}
-          itemScope={true}
-          itemType="http://schema.org/Product"
-          className="col-xs-12 col-md-3 col-sm-6 products-listing__item"
-        >
-          {!!images[0] && (
-            <Media
-              image={images[0]}
-              className="main-image product__main-image"
-              alt={name}
-            />
-          )}
-          <a
-            href={`/product/${slug}`}
-            itemProp="url"
-            className="products-listing__link"
-          >
-            <h3 itemProp="name" className="products-listing__name">
-              {name}
-            </h3>
-          </a>
-          {!!price && (
-            <div className="products-listing__price-block">
-              <span
-                className="price__block"
-                dangerouslySetInnerHTML={{
-                  __html: price_html,
-                }}
-              />
-            </div>
-          )}
-        </li>
-      );
-    });
+    const { images } = item;
 
     return (
-      <ul className="row row--no-gutters products-listing">{listProducts}</ul>
+      <li
+        key={index}
+        itemScope={true}
+        itemType="http://schema.org/Product"
+        className="col-xs-12 col-md-3 col-sm-6 products-listing__item"
+      >
+        {!!images[0] && (
+          <Media
+            image={images[0]}
+            className="main-image product__main-image"
+            alt={name}
+          />
+        )}
+        <a
+          href={`/product/${slug}`}
+          itemProp="url"
+          className="products-listing__link"
+        >
+          <h3 itemProp="name" className="products-listing__name">
+            {name}
+          </h3>
+        </a>
+        {!!price && (
+          <div className="products-listing__price-block">
+            <span
+              className="price__block"
+              dangerouslySetInnerHTML={{ __html: price_html }}
+            />
+          </div>
+        )}
+      </li>
     );
-  }
-}
+  });
+
+  return (
+    <ul className="row row--no-gutters products-listing">{listProducts}</ul>
+  );
+};
+
+export default ProductsListing;
