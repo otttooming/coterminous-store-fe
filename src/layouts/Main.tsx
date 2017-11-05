5fd817dfb0e9e3944653261ade98341aac9a0e8f
Ott Tooming <OttTooming@gmail.com>
2017-11-05 12:04:48 +0200

    Remove redundant sidebar component

--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import Header from "../components/header/Header";
-import Sidemenu from "../components/sidemenu";
 import Footer from "../components/footer/Footer";
 import { bindActionCreators } from "redux";
 import { initStore, startClock, addCount, serverRenderClock } from "../store";
