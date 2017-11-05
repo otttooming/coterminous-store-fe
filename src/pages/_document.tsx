8ddd8d45c3bec074222b325184a13d2a51986a62
Ott Tooming <OttTooming@gmail.com>
2017-11-05 14:21:10 +0200

    Fix _document types

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,9 +3,9 @@ import Document, { Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
+  static getInitialProps({ renderPage }: any) {
     const sheet = new ServerStyleSheet();
-    const page = renderPage(App => props =>
+    const page = renderPage((App: any) => (props: any) =>
       sheet.collectStyles(<App {...props} />)
     );
     const styleTags = sheet.getStyleElement();
@@ -19,7 +19,6 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          <title>My page</title>
           <link rel="stylesheet" href="/static/css/styles.css" />
 
           {this.props.styleTags}
