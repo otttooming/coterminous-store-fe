b744b56875d044e0d84c50a96c951bf28c2bfd98
Ott Tooming <OttTooming@gmail.com>
2018-03-11 17:52:58 +0200

    Create flattenArray function

---
+++ src/common/utils/__tests__/array-flatten.test.ts
@@ -0,0 +1,53 @@
+import flattenArray from "../array-flatten";
+
+const mockObject = {
+  name: "Test",
+};
+
+const mockObjectWithNestedArray = {
+  ...mockObject,
+  nestedArray: [mockObject],
+};
+
+const mockObjectWith3rdLevelNestedArray = {
+  ...mockObject,
+  nestedArray: [
+    {
+      ...mockObject,
+      nestedArray: [mockObject],
+    },
+  ],
+};
+
+const mockArray = [mockObjectWithNestedArray];
+const mockArrayWith3rdLevel = [mockObjectWith3rdLevelNestedArray];
+
+describe("array-flatten", () => {
+  it("should return empty array", () => {
+    expect(flattenArray([], "nestedArray")).toEqual([]);
+  });
+
+  it("should return array with flattened nestedArray", () => {
+    const expected = [mockObjectWithNestedArray, mockObject];
+
+    expect(flattenArray(mockArray, "nestedArray")).toEqual(expected);
+  });
+
+  it("should return array with 3rd level flattened nestedArray", () => {
+    const expected = [
+      mockObjectWith3rdLevelNestedArray,
+      mockObjectWithNestedArray,
+      mockObject,
+    ];
+
+    expect(flattenArray(mockArrayWith3rdLevel, "nestedArray")).toEqual(
+      expected
+    );
+  });
+
+  it("should return array with flattened nestedArray and a primitive", () => {
+    const expected = [mockObjectWithNestedArray, 1, mockObject];
+
+    expect(flattenArray([...mockArray, 1], "nestedArray")).toEqual(expected);
+  });
+});
---
+++ src/common/utils/array-flatten.ts
@@ -0,0 +1,27 @@
+function flatten(cur: any[], nestedArrayName: string): any[] {
+  if (cur.hasOwnProperty(nestedArrayName)) {
+    const currentObject = cur as { [key: string]: any };
+    const nestedArray: any[] = currentObject[nestedArrayName];
+
+    const subCategories = nestedArray.reduce(
+      (acc, cur) => [...acc, ...flatten(cur, nestedArrayName)],
+      []
+    );
+
+    return [...currentObject[nestedArrayName], ...subCategories];
+  }
+
+  return [];
+}
+
+export default function flattenArray(
+  array: any[],
+  nestedArrayName: string
+): any[] {
+  const all: any[] = array.reduce(
+    (acc, cur) => [...acc, ...flatten(cur, nestedArrayName)],
+    array
+  );
+
+  return all;
+}
