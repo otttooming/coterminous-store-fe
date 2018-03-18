8b27f1c725102e7e6c725f40d73d983bb9503e74
Ott Tooming <OttTooming@gmail.com>
2018-03-18 16:00:27 +0200

    Add country codes enum

---
+++ src/common/countries/constants.ts
@@ -0,0 +1,9 @@
+/*
+ * Country codes in ISO 3166-1 alpha-2 format
+ * https://gist.github.com/kyranjamie/646386d5edc174e8b549111572897f81
+*/
+
+export enum Countries {
+  Estonia = "EE",
+  UnitedStates = "US",
+}
