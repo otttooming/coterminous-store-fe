3a2e2d2e119efb631afe3653e3bfaaea79ce372a
Ott Tooming <OttTooming@gmail.com>
2018-03-20 10:23:11 +0200

    Add return type to createOrder request

--- src/services/orderApi/orderApi.ts
+++ src/services/orderApi/orderApi.ts
@@ -1,27 +1,38 @@
 import { SITEURL, WC, buildUrl } from "../api/Api";
 
-import { CreateOrderRequest } from "../../common/woocommerce/typings";
+import {
+  CreateOrderRequest,
+  CreateOrderResponse,
+  WCRestApiError,
+} from "../../common/woocommerce/typings";
 
-export async function createOrder(order: CreateOrderRequest) {
-  const headers = new Headers();
-  headers.append("Accept", "application/json");
-  headers.append("Content-Type", "application/json");
-  headers.append(
-    "Authorization",
-    `Basic ${btoa(SITEURL.consumerKey + ":" + SITEURL.consumerSecret)}`
-  );
+export async function createOrder(
+  order: CreateOrderRequest
+): Promise<CreateOrderResponse | WCRestApiError> {
+  try {
+    const headers = new Headers();
+    headers.append("Accept", "application/json");
+    headers.append("Content-Type", "application/json");
+    headers.append(
+      "Authorization",
+      `Basic ${btoa(SITEURL.consumerKey + ":" + SITEURL.consumerSecret)}`
+    );
 
-  const url = buildUrl({ paths: [WC, "orders"] }, SITEURL);
+    const url = buildUrl({ paths: [WC, "orders"] }, SITEURL);
 
-  const request = await fetch(url, {
-    headers,
-    method: "post",
-    body: JSON.stringify(order),
-  });
+    const request = await fetch(url, {
+      headers,
+      method: "post",
+      body: JSON.stringify(order),
+    });
 
-  const response = request.json();
+    const response: CreateOrderResponse | WCRestApiError = await request.json();
 
-  return response;
+    return response;
+  } catch (e) {
+    // tslint:disable-next-line
+    console.log(e);
+  }
 }
 
 export async function fetchExternalIp() {
