dfb2077324ff7dcef9aa9d9e821d5265e1c5316b
Ott Tooming <OttTooming@gmail.com>
2018-03-20 10:21:30 +0200

    Add CreateOrderResponse WC typings

--- src/common/woocommerce/typings.ts
+++ src/common/woocommerce/typings.ts
@@ -1,3 +1,4 @@
+import { Billing } from "./typings";
 import { Countries } from "./../countries/constants";
 
 export interface Billing {
@@ -44,4 +45,124 @@ export interface CreateOrderRequest {
   shipping?: Shipping;
   line_items: LineItems[];
   shipping_lines: ShippingLines[];
+}
+
+export interface CreateOrderResponse {
+  id: number;
+  parent_id: number;
+  number: string;
+  order_key: string;
+  created_via: string;
+  version: string;
+  status: string;
+  currency: string;
+  date_created: string;
+  date_created_gmt: string;
+  date_modified: string;
+  date_modified_gmt: string;
+  discount_total: string;
+  discount_tax: string;
+  shipping_total: string;
+  shipping_tax: string;
+  cart_tax: string;
+  total: string;
+  total_tax: string;
+  prices_include_tax: boolean;
+  customer_id: number;
+  customer_ip_address: string;
+  customer_user_agent: string;
+  customer_note: string;
+  billing: Billing;
+  shipping: Shipping;
+  payment_method: string;
+  payment_method_title: string;
+  transaction_id: string;
+  date_paid: string;
+  date_paid_gmt: string;
+  date_completed: null;
+  date_completed_gmt: null;
+  cart_hash: string;
+  meta_data: MetaData[];
+  line_items: CreateOrderResponseLine[];
+  tax_lines: TaxLine[];
+  shipping_lines: CreateOrderResponseShippingLine[];
+  fee_lines: any[];
+  coupon_lines: any[];
+  refunds: any[];
+  _links: Links;
+}
+
+export interface Links {
+  self: Collection[];
+  collection: Collection[];
+}
+
+export interface Collection {
+  href: string;
+}
+
+export interface CreateOrderResponseLine {
+  id: number;
+  name: string;
+  product_id: number;
+  variation_id: number;
+  quantity: number;
+  tax_class: string;
+  subtotal: string;
+  subtotal_tax: string;
+  total: string;
+  total_tax: string;
+  taxes: Tax[];
+  meta_data: MetaData[];
+  sku: string;
+  price: number;
+}
+
+export interface MetaData {
+  id: number;
+  key: string;
+  value: string;
+}
+
+export interface Tax {
+  id: number;
+  total: string;
+  subtotal: string;
+}
+
+export interface CreateOrderResponseShippingLine {
+  id: number;
+  method_title: string;
+  method_id: string;
+  total: string;
+  total_tax: string;
+  taxes: any[];
+  meta_data: any[];
+}
+
+export interface TaxLine {
+  id: number;
+  rate_code: string;
+  rate_id: number;
+  label: string;
+  compound: boolean;
+  tax_total: string;
+  shipping_tax_total: string;
+  meta_data: any[];
+}
+
+export interface WCRestApiError {
+  code: "woocommerce_rest_term_invalid";
+  message: string;
+  data: WCRestApiErrorData;
+}
+
+export interface WCRestApiErrorData {
+  status: number;
+}
+
+export function isWCRestApiError<T>(
+  response: WCRestApiError | T
+): response is WCRestApiError {
+  return !response ? false : (<WCRestApiError>response).code !== undefined;
 }
