fc826ab62e3e068c44bfdfc9ca0b40914b25735d
Ott Tooming <OttTooming@gmail.com>
2018-05-15 18:24:18 +0300

    Refactor AdditionalInformation to use FormProvider

--- src/components/contactForm/children/AdditionalInformation.tsx
+++ src/components/contactForm/children/AdditionalInformation.tsx
@@ -1,16 +1,13 @@
 import * as React from "react";
-import { Field } from "redux-form";
-import { FormSectionFieldset } from "../../../common/styles/fieldset";
+import { Section, Heading, TextInputField } from "coterminous-styled";
 
 const AdditionalInformation = () => {
   return (
-    <FormSectionFieldset>
-      <h2>Additional Information</h2>
-      <p>
-        <label htmlFor="comments">Order Notes</label>
-        <Field id="comments" name="comments" component="textarea" type="text" />
-      </p>
-    </FormSectionFieldset>
+    <Section>
+      <Heading.H2>Order notes</Heading.H2>
+
+      <TextInputField.TextArea label={null} name="comments" />
+    </Section>
   );
 };
 
