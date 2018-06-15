d03f81ee86b94931ae7b0cf4231783d9aa05a530
Ott Tooming <OttTooming@gmail.com>
2018-06-15 12:35:09 +0300

    Refactor ShippingMethodInternal to class React.Component

--- src/components/shippingMethods/ShippingMethods.tsx
+++ src/components/shippingMethods/ShippingMethods.tsx
@@ -6,40 +6,42 @@ interface ShippingMethodsProps {
   shippingLocations: ShippingLocations | undefined;
 }
 
-const ShippingMethodInternal = (props: ShippingMethodsProps) => {
-  const { shippingLocations } = props;
+class ShippingMethodInternal extends React.Component<ShippingMethodsProps, {}> {
+  render() {
+    const { shippingLocations } = this.props;
 
-  return !shippingLocations ? null : (
-    <Section>
-      <Heading.H1>Shipping</Heading.H1>
+    return !shippingLocations ? null : (
+      <Section>
+        <Heading.H1>Shipping</Heading.H1>
 
-      <ul className="list-style-none">
-        {shippingLocations.omniva && (
-          <li>
-            <h3>Omniva</h3>
-            {/* <Field
-              name="shipping_method_location"
-              component={DropdownMenu}
-              options={shippingLocations.omniva}
-              className="shipping_method"
-            /> */}
-          </li>
-        )}
+        <ul className="list-style-none">
+          {shippingLocations.omniva && (
+            <li>
+              <h3>Omniva</h3>
+              {/* <Field
+                name="shipping_method_location"
+                component={DropdownMenu}
+                options={shippingLocations.omniva}
+                className="shipping_method"
+              /> */}
+            </li>
+          )}
 
-        {shippingLocations.smartpost && (
-          <li>
-            <h3>SmartPost</h3>
-            {/* <Field
-              name="shipping_method_location"
-              component={DropdownMenu}
-              options={shippingLocations.smartpost}
-              className="shipping_method"
-            /> */}
-          </li>
-        )}
-      </ul>
-    </Section>
-  );
-};
+          {shippingLocations.smartpost && (
+            <li>
+              <h3>SmartPost</h3>
+              {/* <Field
+                name="shipping_method_location"
+                component={DropdownMenu}
+                options={shippingLocations.smartpost}
+                className="shipping_method"
+              /> */}
+            </li>
+          )}
+        </ul>
+      </Section>
+    );
+  }
+}
 
 export default ShippingMethodInternal;
