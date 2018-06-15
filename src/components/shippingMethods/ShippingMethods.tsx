e6921190255c17f9061eecd556baaa0ef1ac85fe
Ott Tooming <OttTooming@gmail.com>
2018-06-15 13:00:53 +0300

    Add Select dropdown for selecting shipping locations

--- src/components/shippingMethods/ShippingMethods.tsx
+++ src/components/shippingMethods/ShippingMethods.tsx
@@ -1,6 +1,14 @@
 import * as React from "react";
-import { ShippingLocations } from "../../services/shippingApi/shippingApi";
-import { Section, Heading } from "coterminous-styled";
+import {
+  ShippingLocations,
+  ShippingLocationItems,
+} from "../../services/shippingApi/shippingApi";
+import {
+  Section,
+  Heading,
+  SelectField,
+  SelectItemProps,
+} from "coterminous-styled";
 
 interface ShippingMethodsProps {
   shippingLocations: ShippingLocations | undefined;
@@ -10,6 +18,12 @@ class ShippingMethodInternal extends React.Component<ShippingMethodsProps, {}> {
   render() {
     const { shippingLocations } = this.props;
 
+    if (!shippingLocations) {
+      return null;
+    }
+
+    const { omniva, smartpost } = shippingLocations;
+
     return !shippingLocations ? null : (
       <Section>
         <Heading.H1>Shipping</Heading.H1>
@@ -18,29 +32,51 @@ class ShippingMethodInternal extends React.Component<ShippingMethodsProps, {}> {
           {shippingLocations.omniva && (
             <li>
               <h3>Omniva</h3>
-              {/* <Field
-                name="shipping_method_location"
-                component={DropdownMenu}
-                options={shippingLocations.omniva}
-                className="shipping_method"
-              /> */}
+
+              <SelectField
+                label={null}
+                name="shipping_method_location_omniva"
+                items={this.renderSelectSection(omniva)}
+              />
             </li>
           )}
 
           {shippingLocations.smartpost && (
             <li>
               <h3>SmartPost</h3>
-              {/* <Field
-                name="shipping_method_location"
-                component={DropdownMenu}
-                options={shippingLocations.smartpost}
-                className="shipping_method"
-              /> */}
+
+              <SelectField
+                label={null}
+                name="shipping_method_location_smartpost"
+                items={this.renderSelectSection(smartpost)}
+              />
             </li>
           )}
         </ul>
       </Section>
     );
+  }
+
+  renderSelectSection(items: ShippingLocationItems[]) {
+    return !items
+      ? null
+      : items.reduce((acc, cur) => {
+          const state = cur.locations.map((location, index) => {
+            return {
+              label: `${location.name}`,
+              renderContent: (
+                <li key={index}>
+                  <div>{location.name}</div>
+                  <div>{location.state}</div>
+                </li>
+              ),
+              value: location.name,
+              searchTerms: [location.name, location.state],
+            };
+          });
+
+          return [...acc, ...state];
+        }, []);
   }
 }
 
