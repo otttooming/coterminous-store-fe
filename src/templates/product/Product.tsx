e5f77873306d08ecc8600533cce4b0972d4588ca
Ott Tooming <OttTooming@gmail.com>
2019-12-27 14:29:41 +0200

    Refactor Product to use chakra-ui components

--- src/templates/product/Product.tsx
+++ src/templates/product/Product.tsx
@@ -4,35 +4,11 @@ import Main from "../../layouts/Main";
 import { ProductTemplateQuery } from "../../generated-models";
 //@ts-ignore
 import { useCart } from "react-use-cart";
-import styled from "styled-components";
-import { Button, Heading, Image } from "@chakra-ui/core";
+import { Button, Heading, Grid, Image, Box } from "@chakra-ui/core";
 
 interface Props {
   data: ProductTemplateQuery;
 }
-
-const Grid = styled.div`
-  display: grid;
-  margin-top: var(--space-m);
-  grid-gap: var(--space-xl);
-  grid-template-columns: repeat(5, 1fr);
-`;
-
-const GridItemIllustration = styled.div`
-  grid-column: 1/6;
-
-  @media (min-width: 960px) {
-    grid-column: 1/3;
-  }
-`;
-
-const GridItemDescription = styled.div`
-  grid-column: 1/6;
-
-  @media (min-width: 960px) {
-    grid-column: 3/6;
-  }
-`;
 
 const ProductTemplate: React.FC<Props> = ({
   data: {
@@ -54,16 +30,20 @@ const ProductTemplate: React.FC<Props> = ({
 
   return (
     <Main hasSidebar={false}>
-      <Grid>
-        <GridItemIllustration>
-          <Image htmlWidth={width} htmlHeight={height} srcSet={srcSet} />
-        </GridItemIllustration>
-        <GridItemDescription>
+      <Grid gap={8} templateColumns="repeat(5, 1fr)">
+        <Image
+          htmlWidth={width}
+          htmlHeight={height}
+          srcSet={srcSet}
+          gridColumn={["1/6", "1/3"]}
+        />
+
+        <Box gridColumn={["1/6", "3/6"]}>
           <Heading as="h2">{name}</Heading>
 
           <div dangerouslySetInnerHTML={{ __html: description }} />
           <Button onClick={() => addItem({ id, price: 1 })}>Add to cart</Button>
-        </GridItemDescription>
+        </Box>
       </Grid>
     </Main>
   );
