20c4fda08d723f56460300f712c0d8313f613862
Ott Tooming <OttTooming@gmail.com>
2019-12-30 10:14:30 +0200

    Fix getLocalizedText to include blocks of translations inside HTML tags

--- src/common/getLocalizedText.ts
+++ src/common/getLocalizedText.ts
@@ -1,3 +1,15 @@
+const getAlphaNumericString = (value: string): string =>
+  value.replace(/[^0-9a-z]/gi, "");
+
+/**
+ * In the text we might have multiple `[:en]text[:]` blocks.
+ */
+const getAllTranslationBlocks = (value: string) => {
+  const regex = /\[:[a-z]{2}](.*?)\[:]/gis;
+
+  return value.match(regex);
+};
+
 const getLanguage = (text: string, language: string): string | null => {
   const regex = {
     any: `\\[:(.*?)]`,
@@ -16,38 +28,61 @@ const getLanguage = (text: string, language: string): string | null => {
    * Match selected @param language
    */
   if (match.selected) {
-    return match.selected[0];
+    return getAlphaNumericString(match.selected[0]);
   }
 
   /**
    * Match any language if existing
    */
   if (match.any) {
-    return match.any[0];
+    return getAlphaNumericString(match.any[0]);
   }
 
   return null;
+};
+
+/**
+ * Get single translated block based on the requested language.
+ */
+const getTranslateBlock = (text: string, textLanguage: string) => {
+  const lang = getLanguage(text, textLanguage);
+
+  if (!lang) {
+    return "";
+  }
+
+  const regex = `\\[:${lang}](.*?)(?=\\[:[a-z]{2}]|\\[:])`;
+
+  const match = text
+    .match(new RegExp(regex, "gis"))![0]
+    .split(`[:${lang}]`)[1]
+    .split("[:")[0];
+
+  return match ? match : "";
 };
 
 export const getLocalizedText = (
   text: string,
   language: string = "en"
 ): string => {
-  const textLanguage = getLanguage(text, language);
+  const translationBlocks = getAllTranslationBlocks(text);
 
   /**
-   * When no result to [:en] or applicable strings are available return full text
+   * When no translations are present, return the full text as is.
    */
-  if (!textLanguage) {
+  if (!translationBlocks) {
     return text;
   }
 
-  const regex = `\\${textLanguage}(.*?)\\[:`;
+  const results = translationBlocks.map(item =>
+    getTranslateBlock(item, language)
+  );
 
-  const match = text
-    .match(new RegExp(regex, "gi"))![0]
-    .split(textLanguage)[1]
-    .split("[:")[0];
+  const outcome = translationBlocks.reduce((acc, cur, index) => {
+    const t = acc.replace(cur, results[index]);
 
-  return match ? match : "";
+    return t;
+  }, text);
+
+  return outcome;
 };
