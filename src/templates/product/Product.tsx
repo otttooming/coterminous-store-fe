cefe985d3e9d26224030e0400d1f922d54a949a3
Ott Tooming <OttTooming@gmail.com>
2019-12-30 16:14:33 +0200

    Fix image missing edge cases for Product

--- src/templates/product/Product.tsx
+++ src/templates/product/Product.tsx
@@ -13,39 +13,39 @@ interface Props {
 
 const ProductTemplate: React.FC<Props> = ({
   data: {
-    cms: {
-      product: {
-        id,
-        name,
-        description,
-        image: {
-          mediaDetails: { width, height },
-          srcSet,
-        },
-      },
-    },
+    cms: { product },
   },
 }) => {
   const { addItem, inCart } = useCart();
   console.log(useCart);
 
+  if (!product) {
+    return null;
+  }
+
+  const { id, name, description, image } = product;
+
   return (
     <Main hasSidebar={false}>
       <Grid gap={8} templateColumns="repeat(5, 1fr)">
-        <Image
-          htmlWidth={width}
-          htmlHeight={height}
-          srcSet={srcSet}
-          gridColumn={["1/6", "1/6", "1/3"]}
-        />
+        {image && image.mediaDetails && (
+          <Image
+            htmlWidth={image.mediaDetails.width || undefined}
+            htmlHeight={image.mediaDetails.height || undefined}
+            srcSet={image.srcSet}
+            gridColumn={["1/6", "1/6", "1/3"]}
+          />
+        )}
 
         <Box gridColumn={["1/6", "1/6", "3/6"]}>
           <Heading as="h2" mb="1rem">
-            {getLocalizedText(name)}
+            {getLocalizedText(name || "")}
           </Heading>
 
           <div
-            dangerouslySetInnerHTML={{ __html: getLocalizedText(description) }}
+            dangerouslySetInnerHTML={{
+              __html: getLocalizedText(description || ""),
+            }}
           />
           <Button onClick={() => addItem({ id, price: 1 })}>Add to cart</Button>
         </Box>
