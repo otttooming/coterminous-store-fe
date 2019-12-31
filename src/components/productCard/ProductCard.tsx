d2d6e940125e545c4f55b58902c15ee40747e9c2
Ott Tooming <OttTooming@gmail.com>
2019-12-31 20:06:12 +0200

    Add price tag to ProductCard

--- src/components/productCard/ProductCard.tsx
+++ src/components/productCard/ProductCard.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { Link } from "gatsby";
-import { Box, Image } from "@chakra-ui/core";
+import { Box, Image, Tag } from "@chakra-ui/core";
 
 export interface ProductCardProps {
   slug: string;
@@ -11,12 +11,24 @@ const ProductCard: React.FC<ProductCardProps> = ({ slug, image, children }) => {
   return (
     <Box as="li" maxW="sm" overflow="hidden">
       <Link to={`/${slug}/`}>
-        <Image
-          htmlWidth={image.width}
-          htmlHeight={image.height}
-          srcSet={image.srcset}
-          rounded="lg"
-        />
+        <Box position="relative">
+          <Image
+            htmlWidth={image.width}
+            htmlHeight={image.height}
+            srcSet={image.srcset}
+            rounded="lg"
+          />
+
+          <Tag
+            size="sm"
+            position="absolute"
+            bottom="4px"
+            left="4px"
+            background="#322659"
+          >
+            150
+          </Tag>
+        </Box>
 
         <Box
           mt="1"
