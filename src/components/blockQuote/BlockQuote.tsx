047af66af5d8273377ec07f2451cf0858db24c31
Ott Tooming <OttTooming@gmail.com>
2020-01-05 16:55:43 +0200

    Add wrapper style to BlockQuote

--- src/components/blockQuote/BlockQuote.tsx
+++ src/components/blockQuote/BlockQuote.tsx
@@ -1,13 +1,32 @@
 import * as React from "react";
 import { Box, Text } from "@chakra-ui/core";
+import styled from "@emotion/styled";
 
 export interface BlockQuoteProps {}
 
+const Wrapper = styled.blockquote`
+  position: relative;
+  font-size: 16px;
+  border-radius: 4px;
+  padding: 4px 16px;
+  padding-left: 48px;
+
+  &:before {
+    content: "\“";
+    position: absolute;
+    left: 16px;
+    font-size: 40px;
+    top: 50%;
+    height: 40px;
+    transform: translateY(-50%);
+  }
+`;
+
 const BlockQuote: React.SFC<BlockQuoteProps> = ({ children }) => {
   return (
-    <Box as="blockquote">
+    <Wrapper>
       <Text children={children} />
-    </Box>
+    </Wrapper>
   );
 };
 
